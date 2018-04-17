e1bcf3ba7f655e75123783bd32c8635676048374
Tiago Pereira <tiagogomespereira@gmail.com>
2018-04-17 11:23:24 +0200

    Basic controllers created
    Created the controller classes to each view controller in the storyboard.

---
+++ TheCoffeeApp/TheCoffeeApp/Controllers/CoffeeShopDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CoffeeShopDetailsViewController.swift
+//  TheCoffeeApp
+//
+//  Created by Tiago Pereira on 17/04/2018.
+//  Copyright © 2018 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeShopDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
